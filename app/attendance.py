c893abfd4d3663412a5e5f9a71872cbd06b3487e
zensajnani <zen@sajnani.com>
2020-09-24 22:04:00 +0530

    adapted code to new MS Teams attendance format

--- app/attendance.py
+++ app/attendance.py
@@ -10,15 +10,6 @@ def calculate_attendance(df, end_time):
     df2['Name'] = students_present
 
     student_attendance = []
-    time_format = '%m/%d/%Y, %I:%M:%S %p'
-
-    #Every browser has a different time format
-    browser_time_format = '%H:%M'
-
-    #Convert time returned from browser into format similar to MS Teams attendance sheet
-    time2 = datetime.strptime(end_time, browser_time_format).strftime('%I:%M:%S %p')
-
-    
 
     # get meeting start time from first row in attendance sheet
     meeting_start_time = df['Timestamp'][0]
@@ -27,9 +18,24 @@ def calculate_attendance(df, end_time):
     time_split = meeting_start_time.split(',')
     meeting_date = time_split[0]
 
-    print(f' END TIME:{end_time}')
+    #Every browser has a different time format
+    browser_time_format = '%H:%M'
+    
+    #Find time format used in sheet
+    try:
+        time_format = '%d/%m/%Y, %H:%M:%S'
+        time_test = datetime.strptime(meeting_start_time, time_format)
+        #Convert time returned from browser into format similar to MS Teams attendance sheet
+        time2 = datetime.strptime(end_time, browser_time_format).strftime('%H:%M:%S')
+    except:
+        time_format = '%m/%d/%Y, %I:%M:%S %p'
+        time_test = datetime.strptime(meeting_start_time, time_format)
+        #Convert time returned from browser into format similar to MS Teams attendance sheet
+        time2 = datetime.strptime(end_time, browser_time_format).strftime('%I:%M:%S %p')
 
+    
     meeting_end_time = meeting_date + ", " + time2
+    print(f' END TIME:{end_time}')
     print(f'MEETING END TIME:{meeting_end_time}')
     meeting_start_time = datetime.strptime(meeting_start_time, time_format)
     meeting_end_time = datetime.strptime(meeting_end_time, time_format)
